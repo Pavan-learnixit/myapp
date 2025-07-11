362530ea166213dffbe2b9637afe4d0b59f330d5
pavan <Pavan.27@learnixit.com>
2025-07-11 12:41:16 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'splash_screen.dart'; // 👈 Add splash screen import
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const SplashScreen(), // 👈 Set splash screen as initial screen
+    );
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+// import 'home_page.dart';
+// import 'backup_screen.dart';
+// import 'profile_page.dart';
+import 'setting_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Navigate to Home Page after 3 seconds
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) =>  SettingsScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/splash.png', // 👈 Your logo path
+          width: 150,         // Optional: set size
+          height: 150,
+        ),
+      ),
+    );
+  }
+}
